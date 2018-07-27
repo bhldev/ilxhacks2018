d0a638ada966e73d2486f1ef9978e6f5ecccb53a
Brian Lim <brian.lim@intelex.com>
2018-07-27 10:35:07 -0400

    JIRA functions

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var circularjson = require('circular-json');
 
 app.listen(8080);
 
+const LOGIN = 'brian.lim@intelex.com';
+const PASSWORD = 'U7IAGCq6YNwgPcsH2u5DEDD9';
+
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
   function (err, data) {
@@ -24,6 +27,17 @@ io.on('connection', function (socket) {
   socket.on('test', function (data) {
     console.log('data');
     socket.emit('test receive', { data: 'testing' });
+  });
+
+  socket.on('jira-request', function (data) {
+    console.log('request:' + JSON.stringify(data));
+    axios.get(data.url, { auth: { username: LOGIN, password: PASSWORD } })
+      .then(response => {
+        console.log('response:' + circularjson.stringify(response));
+        socket.emit('jira-response', { type: data.type, data: circularjson.stringify(response) });
+      }).catch(error => {
+        console.log('error:' + error);
+      });
   });
 
 });
